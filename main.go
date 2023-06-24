93b2c8564940c86a3d1067cae4d526dc496296aa
Liam Maguire <liammaguiresoftware@outlook.com>
2023-06-24 10:29:15 +1000

    Use this

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+func main() {
+	var flags flag.FlagSet
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		// this enables optional fields to be supported.
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+
+		return nil
+	})
+}
